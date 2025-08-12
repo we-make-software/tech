50691d9edd29287a15b19617357db5b7d39348fe
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-08-12 11:20:34 +0200

    Update Run.h

--- Run/Run.h
+++ Run/Run.h
@@ -3,8 +3,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/slab.h>
 
-#define Run(description) MODULE_DESCRIPTION(#description);MODULE_LICENSE("GPL");MODULE_AUTHOR("We-Make-Software.Com");static int __init RunInit(void){DefaultStart();return 0;}static void __exit RunExit(void){DefaultEnd();}module_init(RunInit);module_exit(RunExit)
- 
-#endif    
+#define Run(description)\
+ MODULE_DESCRIPTION(#description);\
+ MODULE_LICENSE("GPL");\
+ MODULE_AUTHOR("We-Make-Software.Com");\
+ static int __init RunInit(void){\
+  DefaultStart();\
+  return 0;\
+ }\
+ static void __exit RunExit(void){\
+  DefaultEnd();\
+ }\
+ module_init(RunInit);\
+ module_exit(RunExit)
+
+#define Void static void
+
+#endif
