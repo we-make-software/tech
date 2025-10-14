c9e32e78bc1a3eb5ebd2a2ce7bf2245e371ef1fa
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-10-14 19:37:47 +0200

    Update .h

--- TaskHandler/.h
+++ TaskHandler/.h
@@ -2,6 +2,8 @@
 #define TaskHandler_h
 #include "../Run/Run.h"
 
+#define WMSR(description, projectEnd, ...) WeMakeSoftwareRun(description, projectEnd, __VA_ARGS__)
+
 #define WMSNAF(name, function) .name = function
 
 #define WMSF(function) .##function = function
