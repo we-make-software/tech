fdad49e064b5fa4742ffd5a86490df5e87fe7787
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-10-16 14:06:36 +0200

    Update .c

--- TaskHandler/.c
+++ TaskHandler/.c
@@ -1,5 +1,13 @@
 #define TaskHandler_h  
 #include "../.h"
+
+struct TaskHandler_Worker{
+  u16 Receive,Transmit;
+  u64 Other; 
+};
+
+
+
 WMSR(TaskHandler,NULL,FUNCTIONS COMMING LATER){
   //Start
 }
